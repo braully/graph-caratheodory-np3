36c8b26473f743ecc9c6df6eda79278ec6f0aa64
braully <braully@gmail.com>
2016-10-10 11:01:43 -0300

    refactor and documentation

--- src/main/java/com/github/braully/graph/operation/GraphCaratheodoryExistsSetOfSize.java
+++ src/main/java/com/github/braully/graph/operation/GraphCaratheodoryExistsSetOfSize.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.github.braully.graph.operation;
 
 import com.github.braully.graph.UndirectedSparseGraphTO;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import org.apache.commons.math3.util.CombinatoricsUtils;
 
 /**
  *
