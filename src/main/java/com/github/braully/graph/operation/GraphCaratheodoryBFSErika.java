6f4f5f6fd0ecf65e1964af0c1b55c816803369f1
braully <braully@gmail.com>
2017-04-24 14:30:38 -0300

    correções algoritmo

--- src/main/java/com/github/braully/graph/operation/GraphCaratheodoryBFSErika.java
+++ src/main/java/com/github/braully/graph/operation/GraphCaratheodoryBFSErika.java
@@ -26,191 +26,176 @@ public class GraphCaratheodoryBFSErika
 
 //    public static boolean verbose = false;
     @Override
-    public Map<String, Object> doOperation(UndirectedSparseGraphTO<Integer, Integer> graphRead) {
-        long totalTimeMillis = -1;
-
-        totalTimeMillis = System.currentTimeMillis();
-        Set<Integer> caratheodorySet = buildMaxCaratheodorySet(graphRead);
-        totalTimeMillis = System.currentTimeMillis() - totalTimeMillis;
-
-        /* Processar a buscar pelo caratheodoryset e caratheodorynumber */
-        Map<String, Object> response = new HashMap<>();
-        if (!caratheodorySet.isEmpty()) {
-            graphRead.setSet(caratheodorySet);
-            response = super.doOperation(graphRead);
-        }
-        return response;
-    }
-
-    public Set<Integer> buildMaxCaratheodorySet(UndirectedSparseGraphTO<Integer, Integer> graph) {
-        BFSDistanceLabeler<Integer, Integer> bdl = new BFSDistanceLabeler<>();
-        Set<Integer> caratheodorySet = new HashSet<>();
-        Collection<Integer> vertices = graph.getVertices();
-        int vertexCount = graph.getVertexCount();
+    public Map<String, Object> doOperation(UndirectedSparseGraphTO<Integer, Integer> graph) {
 
         int maxmaxcg = 0;
 
         System.out.printf("V(G)    = {");
-        for (int i = 0; i < vertexCount; i++) {
+        for (int i = 0; i < graph.getVertexCount(); i++) {
             System.out.printf("%2d | ", i);
         }
         System.out.println("}");
+        Collection<Integer> vertices = graph.getVertices();
 
         for (Integer v : vertices) {
             int maxcg = 0;
-
-            int[] lv = new int[vertexCount];
-            int[] lvLinha = new int[vertexCount];
-            int[] l1v = new int[vertexCount];
-            int[] l2v = new int[vertexCount];
-            int[] l3v = new int[vertexCount];
-
-            System.out.println("0 - Enraizando: " + v);
-
-            //BFS
-            bdl.labelDistances(graph, v);
-            System.out.printf("bfs(%2d) = {", v);
-            for (int i = 0; i < vertexCount; i++) {
-                int distance = bdl.getDistance(graph, i);
-                System.out.printf("%2d | ", distance);
-            }
-            System.out.println("}");
-
-            //Computar arvore
-            Map<Integer, Integer> predecessorCount = new HashMap<>();
-            Map<Integer, Set<Integer>> childs = new HashMap<>();
-            for (int w = 0; w < vertexCount; w++) {
-                Set<Integer> predecessors = bdl.getPredecessors(w);
-                if (predecessors != null) {
-                    for (Integer vp : predecessors) {
-                        Integer cont = predecessorCount.get(vp);
-                        if (cont == null) {
-                            cont = 0;
-                        }
-                        cont = cont + 1;
-                        predecessorCount.put(vp, cont);
-
-                        Set<Integer> chvp = childs.get(vp);
-                        if (chvp == null) {
-                            chvp = new HashSet<>();
-                            childs.put(vp, chvp);
-                        }
-                        chvp.add(w);
-                    }
-                }
-            }
-
-            //Folhas
-            Set<Integer> keySet = predecessorCount.keySet();
-            Set<Integer> leafs = new HashSet<>(vertices);
-            leafs.removeAll(keySet);
-            System.out.println("Leafs(" + v + "): " + leafs);
-
-            // 1 - Se w é folha
-            for (Integer w : leafs) {
-                System.out.println("1 - É folha: " + w);
-                lv[w] = 1;
-                lvLinha[w] = Integer.MIN_VALUE;
-
-            }
-
-            // 2 - Se w tem no maximo um filho u
-            for (int w = 0; w < vertexCount; w++) {
-                Integer pdcount = predecessorCount.get(w);
-                if (pdcount != null && pdcount.equals(1)) {
-                    Set<Integer> ChvW = childs.get(w);
-                    System.out.println("2 - Apenas 1 filho: " + w + " -> " + ChvW);
-                    for (Integer u : ChvW) {
-                        lv[w] = 1;
-                        lvLinha[w] = lv[u]; //duvida
-                    }
-                }
-            }
-
-            // 3 - Se w tem pelo menos dois filho u1, u2
-            for (int w = 0; w < vertexCount; w++) {
-                Integer pdcount = predecessorCount.get(w);
-                if (pdcount != null && pdcount.intValue() >= 2) {
-                    Set<Integer> su = childs.get(w);
-                    System.out.println("3 - Pelo menos 2 filhos: " + w + " -> " + su);
-
-                    // 3a - Se dois filhos são folha
-                    int contFilhosFolha = 0;
-                    for (Integer u : su) {
-                        if (leafs.contains(u)) {
-                            contFilhosFolha++;
-                        }
-                    }
-                    if (contFilhosFolha >= 2) {
-                        System.out.println("\t3a - 2 filhos folha: " + w);
-                        l1v[w] = 2;
-                    }
-
-                    //3b - Conjunto de componentes conexas do subgrafo induzido G|Chv(w)
-                    UndirectedSparseGraphTO inducedSubgraph = FilterUtils.createInducedSubgraph(su, graph);
-                    System.out.println("\t3b - subgrafo induzido G|Chv(w) : " + inducedSubgraph);
-                    List<UndirectedSparseGraphTO> componentesConexas = listaComponentesConexas(inducedSubgraph);
-                    System.out.println("\t Compomentes conexas (" + componentesConexas.size() + "): " + componentesConexas);
-
-                    //3c -  
-                    l2v[w] = Integer.MIN_VALUE;
-                    if (componentesConexas.size() >= 2) {
-                        int maxlvu[] = new int[componentesConexas.size()];
-                        for (int c = 0; c < componentesConexas.size(); c++) {
-                            maxlvu[c] = Integer.MIN_VALUE;
-                            UndirectedSparseGraphTO componente = componentesConexas.get(c);
-                            for (Integer u : (Collection<Integer>) componente.getVertices()) {
-                                maxlvu[c] = Math.max(maxlvu[c], lv[u]);
-                            }
-                        }
-                        Arrays.sort(maxlvu);
-                        l2v[w] = maxlvu[maxlvu.length - 1] + maxlvu[maxlvu.length - 2];
-                    }
-
-                    //3d - 
-                    int maxlvu1lvu2 = Integer.MIN_VALUE;
-                    for (int c = 0; c < componentesConexas.size(); c++) {
-                        int u1 = -1;
-                        int maxlvu = Integer.MIN_VALUE;
-                        int maxlvulinha = Integer.MIN_VALUE;
-                        UndirectedSparseGraphTO componente = componentesConexas.get(c);
-                        for (Integer u : (Collection<Integer>) componente.getVertices()) {
-                            if (maxlvu < lv[u]) {
-                                maxlvu = lv[u];
-                                u1 = u;
-                            }
-                            if (maxlvulinha < lvLinha[u] && !u.equals(u1)) {
-                                maxlvulinha = lvLinha[u];
-                            }
-                        }
-                        if (maxlvu > maxlvu1lvu2 && maxlvulinha > maxlvu1lvu2 && (maxlvu + maxlvulinha) > maxlvu1lvu2) {
-                            maxlvu1lvu2 = maxlvu + maxlvulinha;
-                        }
-                    }
-                    l3v[w] = maxlvu1lvu2;
-
-                    //3e - lv(w) = max{l1v(w), l2v(w), l3v(w)}, lvlinha(w) = max{lv(u) |uEChv(w)}
-                    lv[w] = Math.max(Math.max(l1v[w], l2v[w]), l3v[w]);
-                    lvLinha[w] = Integer.MIN_VALUE;
-                    for (Integer u : su) {
-                        lvLinha[w] = Math.max(lvLinha[w], lv[u]);
-                    }
-                }
-            }
-
-            //4 - c(G) = max{lv(v)|vEV(G)}
-            for (Integer vert : vertices) {
-                if (lv[vert] > maxcg) {
-                    maxcg = lv[vert];
-                }
-            }
-            System.out.println("4 - c(G) = max{lv(v)|vEV(G) = " + maxcg);
+            maxcg = ncaratBfsBloco(graph, v);
             if (maxmaxcg < maxcg) {
                 maxmaxcg = maxcg;
             }
         }
         System.out.println("c(G) = " + maxmaxcg);
-        return caratheodorySet;
+        /* Processar a buscar pelo caratheodoryset e caratheodorynumber */
+        Map<String, Object> response = new HashMap<>();
+        response.put(OperationConvexityGraphResult.PARAM_NAME_CARATHEODORY_NUMBER, maxmaxcg);
+        return response;
+    }
+
+    public int ncaratBfsBloco(UndirectedSparseGraphTO<Integer, Integer> graph, Integer v) {
+        BFSDistanceLabeler<Integer, Integer> bdl = new BFSDistanceLabeler<>();
+        Collection<Integer> vertices = graph.getVertices();
+        int vertexCount = graph.getVertexCount();
+
+        int[] lv = new int[vertexCount];
+        int[] lvLinha = new int[vertexCount];
+        int[] l1v = new int[vertexCount];
+        int[] l2v = new int[vertexCount];
+        int[] l3v = new int[vertexCount];
+        int maxcg = 0;
+        System.out.println("0 - Enraizando: " + v);
+        //BFS
+        bdl.labelDistances(graph, v);
+        System.out.printf("bfs(%2d) = {", v);
+        for (int i = 0; i < vertexCount; i++) {
+            int distance = bdl.getDistance(graph, i);
+            System.out.printf("%2d | ", distance);
+        }
+        System.out.println("}");
+        //Computar arvore
+        Map<Integer, Integer> predecessorCount = new HashMap<>();
+        Map<Integer, Set<Integer>> childs = new HashMap<>();
+        for (int w = 0; w < vertexCount; w++) {
+            Set<Integer> predecessors = bdl.getPredecessors(w);
+            if (predecessors != null) {
+                for (Integer vp : predecessors) {
+                    Integer cont = predecessorCount.get(vp);
+                    if (cont == null) {
+                        cont = 0;
+                    }
+                    cont = cont + 1;
+                    predecessorCount.put(vp, cont);
+
+                    Set<Integer> chvp = childs.get(vp);
+                    if (chvp == null) {
+                        chvp = new HashSet<>();
+                        childs.put(vp, chvp);
+                    }
+                    chvp.add(w);
+                }
+            }
+        }
+        //Folhas
+        Set<Integer> keySet = predecessorCount.keySet();
+        Set<Integer> leafs = new HashSet<>(vertices);
+        leafs.removeAll(keySet);
+        System.out.println("Leafs(" + v + "): " + leafs);
+        // 1 - Se w é folha
+        for (Integer w : leafs) {
+            System.out.println("1 - É folha: " + w);
+            lv[w] = 1;
+            lvLinha[w] = Integer.MIN_VALUE;
+
+        }
+        // 2 - Se w tem no maximo um filho u
+        for (int w = 0; w < vertexCount; w++) {
+            Integer pdcount = predecessorCount.get(w);
+            if (pdcount != null && pdcount.equals(1)) {
+                Set<Integer> ChvW = childs.get(w);
+                System.out.println("2 - Apenas 1 filho: " + w + " -> " + ChvW);
+                for (Integer u : ChvW) {
+                    lv[w] = 1;
+                    lvLinha[w] = lv[u]; //duvida
+                }
+            }
+        }
+        // 3 - Se w tem pelo menos dois filho u1, u2
+        for (int w = 0; w < vertexCount; w++) {
+            Integer pdcount = predecessorCount.get(w);
+            if (pdcount != null && pdcount.intValue() >= 2) {
+                Set<Integer> su = childs.get(w);
+                System.out.println("3 - Pelo menos 2 filhos: " + w + " -> " + su);
+
+                // 3a - Se dois filhos são folha
+                int contFilhosFolha = 0;
+                for (Integer u : su) {
+                    if (leafs.contains(u)) {
+                        contFilhosFolha++;
+                    }
+                }
+                if (contFilhosFolha >= 2) {
+                    System.out.println("\t3a - 2 filhos folha: " + w);
+                    l1v[w] = 2;
+                }
+
+                //3b - Conjunto de componentes conexas do subgrafo induzido G|Chv(w)
+                UndirectedSparseGraphTO inducedSubgraph = FilterUtils.createInducedSubgraph(su, graph);
+                System.out.println("\t3b - subgrafo induzido G|Chv(w) : " + inducedSubgraph);
+                List<UndirectedSparseGraphTO> componentesConexas = listaComponentesConexas(inducedSubgraph);
+                System.out.println("\t Compomentes conexas (" + componentesConexas.size() + "): " + componentesConexas);
+
+                //3c -
+                l2v[w] = Integer.MIN_VALUE;
+                if (componentesConexas.size() >= 2) {
+                    int maxlvu[] = new int[componentesConexas.size()];
+                    for (int c = 0; c < componentesConexas.size(); c++) {
+                        maxlvu[c] = Integer.MIN_VALUE;
+                        UndirectedSparseGraphTO componente = componentesConexas.get(c);
+                        for (Integer u : (Collection<Integer>) componente.getVertices()) {
+                            maxlvu[c] = Math.max(maxlvu[c], lv[u]);
+                        }
+                    }
+                    Arrays.sort(maxlvu);
+                    l2v[w] = maxlvu[maxlvu.length - 1] + maxlvu[maxlvu.length - 2];
+                }
+
+                //3d -
+                int maxlvu1lvu2 = Integer.MIN_VALUE;
+                for (int c = 0; c < componentesConexas.size(); c++) {
+                    int u1 = -1;
+                    int maxlvu = Integer.MIN_VALUE;
+                    int maxlvulinha = Integer.MIN_VALUE;
+                    UndirectedSparseGraphTO componente = componentesConexas.get(c);
+                    for (Integer u : (Collection<Integer>) componente.getVertices()) {
+                        if (maxlvu < lv[u]) {
+                            maxlvu = lv[u];
+                            u1 = u;
+                        }
+                        if (maxlvulinha < lvLinha[u] && !u.equals(u1)) {
+                            maxlvulinha = lvLinha[u];
+                        }
+                    }
+                    if (maxlvu > maxlvu1lvu2 && maxlvulinha > maxlvu1lvu2 && (maxlvu + maxlvulinha) > maxlvu1lvu2) {
+                        maxlvu1lvu2 = maxlvu + maxlvulinha;
+                    }
+                }
+                l3v[w] = maxlvu1lvu2;
+
+                //3e - lv(w) = max{l1v(w), l2v(w), l3v(w)}, lvlinha(w) = max{lv(u) |uEChv(w)}
+                lv[w] = Math.max(Math.max(l1v[w], l2v[w]), l3v[w]);
+                lvLinha[w] = Integer.MIN_VALUE;
+                for (Integer u : su) {
+                    lvLinha[w] = Math.max(lvLinha[w], lv[u]);
+                }
+            }
+        }
+        //4 - c(G) = max{lv(v)|vEV(G)}
+        for (Integer vert : vertices) {
+            if (lv[vert] > maxcg) {
+                maxcg = lv[vert];
+            }
+        }
+        System.out.println("4 - c(G) = max{lv(v)|vEV(G) = " + maxcg);
+        return maxcg;
     }
 
     @Override
