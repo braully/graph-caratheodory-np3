4c56399d32d0e10cac52a36e8647fa4301f2edc3
strike <strike@alastor>
2018-05-26 22:44:06 -0300

    sort utils.

--- src/test/java/com/github/braully/graph/GraphSkelTest.java
+++ src/test/java/com/github/braully/graph/GraphSkelTest.java
@@ -91,17 +91,42 @@ public class GraphSkelTest extends TestCase {
         int ko = k - 2;
         int len = ((ko + 1) * ko) / 2;
         int arr[] = new int[len];
-        int count[] = new int[len];
+        int arrup[] = new int[len];
+        int arrdown[] = new int[len];
+        int[] countpos = new int[len];
+        int[] countval = new int[ko];
+        int max_val_count = len / ko;
 
         Map<Integer, List<Integer>> possibilidades = new HashMap<>(len);
         Integer[] targetv = new Integer[ko];
 
         for (int j = 0; j < ko; j++) {
             targetv[j] = j;
+            countval[j]++;
         }
 
+        int offsetup = ko - 1;
+        int up = 0;
+        int down = 1;
+        for (int i = 0; i < len; i++) {
+            arrup[i] = up;
+            arrdown[i] = down++;
+            if (i == offsetup) {
+                up++;
+                offsetup += (ko - up);
+            }
+            if (down == ko + 1) {
+                down = up + 1;
+            }
+        }
+
+        System.out.println("\nUp:");
+        printArray(arrup);
+        System.out.println("\nDown:");
+        printArray(arrdown);
+
         for (int i = 0; i < ko; i++) {
-            count[i] = 0;
+            countpos[i] = 0;
             arr[i] = i;
             List<Integer> listaPossiveis = new ArrayList<>(len);
             listaPossiveis.addAll(Arrays.asList(targetv));
@@ -118,26 +143,32 @@ public class GraphSkelTest extends TestCase {
 
         while (pos < len) {
             List<Integer> list = possibilidades.get(pos);
-            if (!list.isEmpty()) {
+            if (countpos[pos] > list.size()) {
                 //rollback
+                break;
             }
-            count[pos]++;
-            int size = list.size() - 1;
-            int idx = (int) Math.round(size * Math.random());
-            int val = list.get(idx);            
+            int lsize = list.size();
+            int idx = countpos[pos];
+            int val = list.get(idx);
+            while ((countval[val] >= max_val_count || exclude(arrup, arrdown, arr, pos, val)) && idx++ <= lsize * 2) {
+                val = list.get(idx++ % lsize);
+            }
+            if (countval[val] >= max_val_count) {
+                //rollback
+                break;
+            }
             arr[pos] = val;
+            countval[val]++;
+            countpos[pos]++;
             pos++;
         }
 
         System.out.println("\nCombinação:");
-        System.out.print("[");
-        for (int i = 0; i < len; i++) {
-            System.out.print(arr[i]);
-            if (i < len - 1) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println("]");
+        printArray(arr);
+    }
+
+    private boolean exclude(int[] arrup, int[] arrdown, int[] arr, int pos, int val) {
+        return false;
     }
 
 //    public static synchronized void nextCombination(int n,
@@ -166,6 +197,17 @@ public class GraphSkelTest extends TestCase {
 //            currentCombination[j + 1] = currentCombination[j] + 1;
 //        }
 //    }
+    private void printArray(int[] arr) {
+        int len = arr.length;
+        System.out.print("[");
+        for (int i = 0; i < len; i++) {
+            System.out.print(arr[i]);
+            if (i < len - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
 }
 
 //        int pos = ko;
